c12e41f62cb91c00b15fcdb8849d5c927ea33837
biddy1618 <biddy.as.diddy@gmail.com>
2022-09-26 15:46:56 +0600

    Minor - crawled data for 2015

--- run.py
+++ run.py
@@ -37,7 +37,10 @@ DATA_FOLDER.mkdir(parents=True, exist_ok=True)
 #     end_date = '01.'+str(month+1).zfill(2)+'.2012'
 #     file_name = Path.joinpath(FOLDER_2012, str(month).zfill(2)+'12.json')
 #     crawl_and_save_to_file(start_date=start_date, end_date=end_date, file_name=file_name)
-# crawl_and_save_to_file(start_date='01.12.2012', end_date='01.01.2013', file_name='./data/1212.json')
+# crawl_and_save_to_file(
+#     start_date='01.12.2012',
+#     end_date='01.01.2013',
+#     file_name=Path.joinpath(FOLDER_2012, '1212.json'))
 
 
 # 2013
@@ -64,14 +67,47 @@ DATA_FOLDER.mkdir(parents=True, exist_ok=True)
 #     end_date = '01.'+str(month+1).zfill(2)+'.2013'
 #     file_name = Path.joinpath(FOLDER_2013, str(month).zfill(2)+'13.json')
 #     crawl_and_save_to_file(start_date=start_date, end_date=end_date, file_name=file_name)
-# crawl_and_save_to_file(start_date='01.12.2013', end_date='01.01.2014', file_name='./data/1213.json')
+# crawl_and_save_to_file(
+#     start_date='01.12.2013',
+#     end_date='01.01.2014',
+#     file_name=Path.joinpath(FOLDER_2013, '1213.json'))
 
 
 # 2014
-FOLDER_2014 = Path.joinpath(DATA_FOLDER, '2014')
-FOLDER_2014.mkdir(parents=True, exist_ok=True)
+# FOLDER_2014 = Path.joinpath(DATA_FOLDER, '2014')
+# FOLDER_2014.mkdir(parents=True, exist_ok=True)
 
-LOG_FILE = Path.joinpath(FOLDER_2014, 'crawler.logs')
+# LOG_FILE = Path.joinpath(FOLDER_2014, 'crawler.logs')
+
+# logging.basicConfig(
+#     format='{levelname} {name} {asctime}: {message}',
+#     level=logging.INFO,
+#     datefmt='%m/%d/%Y %H:%M:%S',
+#     style='{',
+#     handlers=[
+#         logging.FileHandler(LOG_FILE),
+#         logging.StreamHandler()
+#     ]
+# )
+
+# MONTHS = list(range(1, 12))
+
+# for month in MONTHS:
+#     start_date = '01.'+str(month).zfill(2)+'.2014'
+#     end_date = '01.'+str(month+1).zfill(2)+'.2014'
+#     file_name = Path.joinpath(FOLDER_2014, str(month).zfill(2)+'14.json')
+#     crawl_and_save_to_file(start_date=start_date, end_date=end_date, file_name=file_name)
+# crawl_and_save_to_file(
+#     start_date='01.12.2014',
+#     end_date='01.01.2015',
+#     file_name=Path.joinpath(FOLDER_2014, '1214.json'))
+
+
+# 2015
+FOLDER_2015 = Path.joinpath(DATA_FOLDER, '2015')
+FOLDER_2015.mkdir(parents=True, exist_ok=True)
+
+LOG_FILE = Path.joinpath(FOLDER_2015, 'crawler.logs')
 
 logging.basicConfig(
     format='{levelname} {name} {asctime}: {message}',
@@ -87,8 +123,11 @@ logging.basicConfig(
 MONTHS = list(range(1, 12))
 
 for month in MONTHS:
-    start_date = '01.'+str(month).zfill(2)+'.2014'
-    end_date = '01.'+str(month+1).zfill(2)+'.2014'
-    file_name = Path.joinpath(FOLDER_2014, str(month).zfill(2)+'14.json')
+    start_date = '01.'+str(month).zfill(2)+'.2015'
+    end_date = '01.'+str(month+1).zfill(2)+'.2015'
+    file_name = Path.joinpath(FOLDER_2015, str(month).zfill(2)+'15.json')
     crawl_and_save_to_file(start_date=start_date, end_date=end_date, file_name=file_name)
-crawl_and_save_to_file(start_date='01.12.2014', end_date='01.01.2015', file_name='./data/1214.json')
+crawl_and_save_to_file(
+    start_date='01.12.2015',
+    end_date='01.01.2016',
+    file_name=Path.joinpath(FOLDER_2015, '1215.json'))
