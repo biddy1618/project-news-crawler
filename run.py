f93623bdcadf9b3e5d083749fcf78809f98e0308
biddy1618 <biddy.as.diddy@gmail.com>
2022-10-03 18:02:05 +0600

    Minor - crawled data for 2020

--- run.py
+++ run.py
@@ -224,10 +224,40 @@ DATA_FOLDER.mkdir(parents=True, exist_ok=True)
 
 
 # 2019
-FOLDER_2019 = Path.joinpath(DATA_FOLDER, '2019')
-FOLDER_2019.mkdir(parents=True, exist_ok=True)
+# FOLDER_2019 = Path.joinpath(DATA_FOLDER, '2019')
+# FOLDER_2019.mkdir(parents=True, exist_ok=True)
 
-LOG_FILE = Path.joinpath(FOLDER_2019, 'crawler.logs')
+# LOG_FILE = Path.joinpath(FOLDER_2019, 'crawler.logs')
+
+# logging.basicConfig(
+#     format='{levelname} {name} {asctime}: {message}',
+#     level=logging.INFO,
+#     datefmt='%m/%d/%Y %H:%M:%S',
+#     style='{',
+#     handlers=[
+#         logging.FileHandler(LOG_FILE),
+#         logging.StreamHandler()
+#     ]
+# )
+
+# MONTHS = list(range(1, 12))
+
+# for month in MONTHS:
+#     start_date = '01.'+str(month).zfill(2)+'.2019'
+#     end_date = '01.'+str(month+1).zfill(2)+'.2019'
+#     file_name = Path.joinpath(FOLDER_2019, str(month).zfill(2)+'19.json')
+#     crawl_and_save_to_file(start_date=start_date, end_date=end_date, file_name=file_name)
+# crawl_and_save_to_file(
+#     start_date='01.12.2019',
+#     end_date='01.01.2020',
+#     file_name=Path.joinpath(FOLDER_2019, '1219.json'))
+
+
+# 2020
+FOLDER_2020 = Path.joinpath(DATA_FOLDER, '2020')
+FOLDER_2020.mkdir(parents=True, exist_ok=True)
+
+LOG_FILE = Path.joinpath(FOLDER_2020, 'crawler.logs')
 
 logging.basicConfig(
     format='{levelname} {name} {asctime}: {message}',
@@ -243,11 +273,11 @@ logging.basicConfig(
 MONTHS = list(range(1, 12))
 
 for month in MONTHS:
-    start_date = '01.'+str(month).zfill(2)+'.2019'
-    end_date = '01.'+str(month+1).zfill(2)+'.2019'
-    file_name = Path.joinpath(FOLDER_2019, str(month).zfill(2)+'19.json')
+    start_date = '01.'+str(month).zfill(2)+'.2020'
+    end_date = '01.'+str(month+1).zfill(2)+'.2020'
+    file_name = Path.joinpath(FOLDER_2020, str(month).zfill(2)+'20.json')
     crawl_and_save_to_file(start_date=start_date, end_date=end_date, file_name=file_name)
 crawl_and_save_to_file(
-    start_date='01.12.2019',
-    end_date='01.01.2020',
-    file_name=Path.joinpath(FOLDER_2019, '1219.json'))
+    start_date='01.12.2020',
+    end_date='01.01.2021',
+    file_name=Path.joinpath(FOLDER_2020, '1220.json'))
